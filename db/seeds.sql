be8e92f2e5878dac2fcc5f951765a98bb2b57de4
rbenites <rbenites115@gmail.com>
2018-06-01 09:15:19 -0700

    initial commit with foler/files structure

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name) VALUES ('Septuple bi-pass',false);
+INSERT INTO burgers (name) VALUES ('Rest in Peas',false);
+INSERT INTO burgers (name) VALUES ('Fi-eta Bout it',false);
+INSERT INTO burgers (name) VALUES ('Meatsiah',false);
