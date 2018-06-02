7e9ac263d3eb82e78e7bd5a1fdfdb4b2613f65ba
rbenites <rbenites115@gmail.com>
2018-06-02 09:55:56 -0700

    added Schema and Seeds data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO burgers (name) VALUES ('Septuple bi-pass',false);
-INSERT INTO burgers (name) VALUES ('Rest in Peas',false);
-INSERT INTO burgers (name) VALUES ('Fi-eta Bout it',false);
-INSERT INTO burgers (name) VALUES ('Meatsiah',false);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Septuple bi-pass',false);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Rest in Peas',false);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Fi-eta Bout it',false);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Meatsiah',false);
